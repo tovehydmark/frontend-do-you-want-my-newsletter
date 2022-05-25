6596b68ff093c2dd6d65d1ec9bc2c8452ac31ae5
tovehydmark <tovehydmark@gmail.com>
2022-05-25 17:15:22 +0200

    general function to use for fetch

---
+++ src/components/Fetch.js
@@ -0,0 +1,24 @@
+//Fetch function to be reused
+
+export async function Fetch(url, method, body) {
+
+    try {
+        let response = await fetch(url, {
+            method,
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        console.log(response);
+
+        if (response.status === 404) {
+            throw new Error("Not found")
+        }
+
+        return await response.json()
+    } catch (err) {
+        throw err
+    }
+}
