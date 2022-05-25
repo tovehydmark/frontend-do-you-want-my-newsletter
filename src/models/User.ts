58bb7bd98ee510ed38930bfa7281829148ba17f6
tovehydmark <tovehydmark@gmail.com>
2022-05-25 17:31:34 +0200

    includes wants newsletter

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ export class User {
   constructor(
     public username: string,
     public email: string,
-    public password: string
+    public password: string,
+    public wantsNewsLetter: boolean
   ) {}
 }
