5e30ab11f5a5e7a3a6f9c228ddd175485baa0369
tovehydmark <tovehydmark@gmail.com>
2022-05-27 11:51:19 +0200

    Hashrouter used for routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,24 @@ import logo from "./logo.svg";
 import "./App.css";
 import { CreateUser } from "./components/CreateUser";
 import { Login } from "./components/Login";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { NotFound } from "./components/NotFound";
+import { LoggedIn } from "./components/LoggedIn";
 
 function App() {
   return (
     <>
-      <Login></Login>
-      {/* <CreateUser></CreateUser> */}
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Login />} />
+            <Route path="/LoggedIn" element={<LoggedIn />} />
+            <Route path="/CreateUser" element={<CreateUser />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </>
   );
 }
