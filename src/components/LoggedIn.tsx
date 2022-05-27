aa2d0fe84d63d5b5043599a4f60adbd5456a5875
tovehydmark <tovehydmark@gmail.com>
2022-05-27 22:32:57 +0200

    when user is loggedin, data about user fetched

--- src/components/LoggedIn.tsx
+++ src/components/LoggedIn.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserId } from "../models/UserId";
+import { Fetch } from "./Fetch";
 
 export function LoggedIn() {
-  const [loggedInUser, setLoggedInUser] = useState<string>("");
+  const [loggedInUserId, setLoggedInUserId] = useState<string>("");
+
+  const [isLoggedin, setIsLoggedin] = useState(false);
 
   const nav = useNavigate();
 
@@ -13,12 +17,45 @@ export function LoggedIn() {
     if (!userFromLs) {
       nav("/");
     } else {
-      setLoggedInUser(userFromLs);
+      setLoggedInUserId(userFromLs);
+      setIsLoggedin(true);
     }
   }, []);
 
-  //Här är loggedInUser sin sträng
-  console.log(loggedInUser);
+  useEffect(() => {
+    presentUserDetails();
+  }, [isLoggedin]);
+
+  const presentUserDetails = async () => {
+    let userFromLs = localStorage.getItem("loggedInUserId");
+
+    if (userFromLs !== null) {
+      let userId = new UserId(userFromLs);
+
+      let response = await fetch("http://localhost:1337/users/loggedin", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          // Host: "http://localhost:1337",
+        },
+        body: JSON.stringify(userId),
+      }).then((res) =>
+        res.json().then((data) => {
+          console.log(data);
+        })
+      );
+    }
+
+    // await Fetch(
+    //   "http://localhost:1337/users/loggedin",
+    //   "post",
+    //   loggedInUserId
+    // ).then((res) =>
+    //   res.json().then((data: any) => {
+    //     console.log(data);
+    //   })
+    // );
+  };
 
   return (
     <>
