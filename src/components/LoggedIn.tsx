97d3ee274b0f073e280d0771dbd3844ec4659c4c
tovehydmark <tovehydmark@gmail.com>
2022-05-27 14:02:53 +0200

    userId to LS and value controlled in loggedin comp

--- src/components/LoggedIn.tsx
+++ src/components/LoggedIn.tsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function LoggedIn() {
+  const [loggedInUser, setLoggedInUser] = useState<string>("");
+
+  const nav = useNavigate();
+
+  useEffect(() => {
+    let userFromLs = localStorage.getItem("loggedInUserId");
+
+    //If no user is logged in, one is redirected to login page if trying to access /LogedIn route
+    if (!userFromLs) {
+      nav("/");
+    } else {
+      setLoggedInUser(userFromLs);
+    }
+  }, []);
+
+  //Här är loggedInUser sin sträng
+  console.log(loggedInUser);
+
   return (
     <>
       <p>Logged in </p>
