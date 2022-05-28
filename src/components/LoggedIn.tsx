00c2a25105765c11496201a79582ab3bf737a3f9
tovehydmark <tovehydmark@gmail.com>
2022-05-28 12:31:19 +0200

    updates subscription status in db on button click

--- src/components/LoggedIn.tsx
+++ src/components/LoggedIn.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { UpdateSubscriptionStatus } from "../models/UpdateSubscriptionStatus";
 import { User } from "../models/User";
 import { UserId } from "../models/UserId";
 import { IFormInputs } from "../models/UserInterface";
@@ -8,6 +9,8 @@ import { Fetch } from "./Fetch";
 export function LoggedIn() {
   const [loggedInUserId, setLoggedInUserId] = useState<string>("");
   const [loggedInUserName, setLoggedInUserName] = useState<string>("");
+  const [loggedInSubscriptionStatus, setLoggedInSubscriptionStatus] =
+    useState<boolean>(false);
 
   // const [isLoggedin, setIsLoggedin] = useState(false);
 
@@ -45,18 +48,59 @@ export function LoggedIn() {
         console.log(res);
 
         setLoggedInUserName(res[0].username);
+        setLoggedInSubscriptionStatus(res[0].wantsNewsLetter);
 
         return res;
       });
-
-      console.log(response);
-      console.log(response.username);
     }
   };
 
+  async function changeSubscriptionStatus() {
+    setLoggedInSubscriptionStatus(!loggedInSubscriptionStatus);
+
+    let updateUser = new UpdateSubscriptionStatus(
+      loggedInUserId,
+      !loggedInSubscriptionStatus
+    );
+
+    // let response = await fetch(
+    //   "http://localhost:1337/users/updateSubscription",
+    //   {
+    //     method: "put",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(updateUser),
+    //   }
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+
+    await Fetch(
+      "http://localhost:1337/users/updateSubscription",
+      "put",
+      updateUser
+    ).then((data: any) => {
+      console.log(data);
+    });
+  }
+
   return (
     <>
-      <p>Välkommen {loggedInUserName} </p>
+      <div className="loggedInView">
+        <p>Välkommen {loggedInUserName} </p>
+        <p>
+          {loggedInSubscriptionStatus
+            ? "Trött på våra utskick?"
+            : "Vill du ha vårt nyhetsbrev?"}
+        </p>
+        <button onClick={changeSubscriptionStatus}>
+          {loggedInSubscriptionStatus ? "Avprenumerera" : "Prenumerera"}
+        </button>
+        <button>Logga ut</button>
+      </div>
     </>
   );
 }
